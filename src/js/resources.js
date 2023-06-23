001fd6f5355aec33281a38f22451210736d37f21
Rayel2002 <1002893@hr.nl>
2023-06-23 11:55:55 +0200

    added 'press start to play' and logo to homescreen

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,11 +3,15 @@ import { TiledMapResource } from '@excaliburjs/plugin-tiled';
 import taxiImage from "../assets/other-assets/watertaxi.png";
 import testMap from "../assets/level-assets/TestMap.tmx";
 import startBackground from "../assets/other-assets/startBackground.jpg";
+import logo from "../assets/other-assets/Logo.png";
+import start from "../assets/other-assets/Press-Start.png";
 
 const Resources = {
     Taxi: new ImageSource(taxiImage),
     TestMap: new TiledMapResource(testMap),
-    Background: new ImageSource(startBackground)
+    Background: new ImageSource(startBackground),
+    Logo: new ImageSource(logo),
+    Start: new ImageSource(start)
 };
 
 // met deze for loop hoef je niet alles handmatig in de loader te zetten
